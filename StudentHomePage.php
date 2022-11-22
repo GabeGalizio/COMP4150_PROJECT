8f0c0196b74aa83c8de6761f93e54f2268bec060
Gabriel Graziano Galizio <galizio712@gmail.com>
2022-11-22 16:05:21 -0500

    created log in and home pages for each item on the home page

--- StudentHomePage.php
+++ StudentHomePage.php
@@ -4,7 +4,13 @@
 </head>
 <body>
 <h2>Student Home Page</h2>
-
+<ul>
+    <li>Update Personal Info</li>
+    <li>Register for Room Placement</li>
+    <li>View Invoice</li>
+    <li>Make a payment</li>
+    <li>Exit</li>
+</ul>
 </body>
 </html>
 
