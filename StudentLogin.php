2ad8b5578bb813b9349f780991f061a75f1aa682
Dren Jaha <jahad@uwindsor.ca>
2022-11-22 16:36:36 -0500

    Added boilerplate

--- StudentLogin.php
+++ StudentLogin.php
@@ -1,9 +1,14 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Student Login Page</title>
 </head>
 <body>
-<h2>Student log in page</h2>
+<h1>Student Login</h1>
+
 </body>
 </html>
 
