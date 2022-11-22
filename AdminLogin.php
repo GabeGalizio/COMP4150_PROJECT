44974283804403088b0cd76eed155799573a2445
Dren Jaha <jahad@uwindsor.ca>
2022-11-22 16:48:47 -0500

    Added login and password

--- AdminLogin.php
+++ AdminLogin.php
@@ -1,9 +1,21 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Admin Login Page</title>
 </head>
 <body>
-<h2>Admin log in page </h2>
+<h1>Admin Login</h1>
+<h2>Log in to your account</h2>
+<form>
+    <input type="text" title="username" placeholder="username" />
+    <br>
+    <input type="password" title="username" placeholder="password" />
+    <br>
+    <button type="submit" class="btn">Login</button>
+</form>
 </body>
 </html>
 <?php
