9fa71caa4d1d65f35685d8ee437dfab2b6cff761
Gabriel Graziano Galizio <galizio712@gmail.com>
2022-11-22 19:49:19 -0500

    styled the index page

--- index.php
+++ index.php
@@ -3,25 +3,29 @@
     <title>Comp 4120 Phase 3 Project Page</title>
     <link rel="stylesheet" href="indexStyleSheet.css">
 </head>
-<body>
-<h1>Welcome to UWindsor Hostel Staff Managment</h1>
-<div class="mainGrid">
-    <div class="item1">
-        <h3><a href="StudentLogin.php">Student</a></h3>
+<div class="backgroundStyle">
+    <body>
+    <div class="titleStyle">
+        <h1>Welcome to UWindsor Hostel Staff Managment</h1>
+    </div>
+    <div class="mainGrid">
+        <div class="item1">
+            <h3><a href="StudentLogin.php">Student</a></h3>
+        </div>
+
+        <div class="item2">
+            <h3><a href="StaffLogin.php">Staff</a></h3>
+        </div>
+
+        <div class="item3">
+            <h3><a href="AdminLogin.php">Admin</a></h3>
+        </div>
+
+
     </div>
 
-    <div class="item2">
-        <h3><a href="StaffLogin.php">Staff</a></h3>
-    </div>
-
-    <div class="item3">
-        <h3><a href="AdminLogin.php">Admin</a></h3>
-    </div>
-
-
+    </body>
 </div>
-
-</body>
 </html>
 
 
