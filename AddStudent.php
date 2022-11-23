591f21763fab0236b2d4506a16d6028631d5da24
mancinii5 <mancini5@uwindsor.ca>
2022-11-22 21:08:14 -0500

    updated files

---
+++ AddStudent.php
@@ -0,0 +1,58 @@
+<div id="AddStudent">
+    <h2>Add Student</h2>
+    <form action="" method="post">
+        <label>Student Number :</label>
+        <input type="number" name="studentnum" required placeholder="Please Student Number"/><br><br>
+        <label>First Name :</label>
+        <input type="text" name="firstname"/><br><br>
+        <label>Last Name :</label>
+        <input type="text" name="lastname"/><br><br>
+        <label>Address:</label>
+        <input type="text" name="address"/><br><br>
+        <label>Birthdate :</label>
+        <input type="date" name="birthdate"/><br><br>
+        <select name="gender" required>
+            <option value=""> Select Gender </option>
+            <option value="M"> Male </option>
+            <option value="F"> Female </option>
+        </select>
+        <br><br>
+        <label>Academic Year :</label>
+        <input type="text" name="academicyear" /><br><br>
+        <label>Nationality :</label>
+        <input type="text" name="nationality"/><br><br>
+        <label>Special Needs :</label>
+        <input type="text" name="specialneeds" /><br><br>
+        <label>Comments:</label>
+        <input type="text" name="comments" /><br><br>
+        <label>Student Status:</label>
+        <input type="text" name="studentstatus" /><br><br>
+        <label>Course:</label>
+        <input type="text" name="course" /><br><br>
+
+        <br><br>
+        <input type="submit" value=" Submit Details " name="submit"/><br />
+    </form>
+</div>
+
+<?php
+if(isset($_POST["submit"])){
+    include 'dbconfig.php';
+    $sql = "INSERT INTO students (S_NUM, F_NAME, L_NAME, ADDRESS,BIRTHDATE, GENDER,ACADEMIC_YEAR,NATIONALITY,SPECIAL_NEEDS,COMMENTS,S_STATUS,COURSE)
+VALUES ('".$_POST["studentnum"]."','".$_POST["firstname"]."','".$_POST["lastname"]."','".$_POST["address"]."','".$_POST["birthdate"]."','".$_POST["gender"]."','".$_POST["academicyear"]."','".$_POST["nationality"]."','".$_POST["specialneeds"]."','".$_POST["comments"]."','".$_POST["studentstatus"]."','".$_POST["course"]."')";
+    if ($conn->query($sql) === TRUE) {
+        echo "
+ <script type= 'text/javascript'>
+alert('New record created successfully');
+ </script>";
+    }
+    else
+    {
+         echo
+     "<script type= 'text/javascript'>
+ alert('Error: " . $sql . "<br>" . $conn->error."');
+ </script>";
+}
+    $conn->close();
+}
+?>
--- AdminHomePage.php
+++ AdminHomePage.php
@@ -1,1 +1,13 @@
-<?php
+<html>
+<head>
+
+</head>
+<body>
+<h2>Admin Home Page</h2>
+<ul>
+    <li><a href="AddStudent.php">Add Student</li>
+    <li><a href="">Add Staff Member</li>
+    <li><a href="" >Exit</li>
+</ul>
+</body>
+</html>
