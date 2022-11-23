dfb0a7a3ac568e04d5835c5cc244a2ef9b8ae90e
mancinii5 <mancini5@uwindsor.ca>
2022-11-22 21:29:05 -0500

    updated files

--- AdminHomePage.php
+++ AdminHomePage.php
@@ -6,7 +6,7 @@
 <h2>Admin Home Page</h2>
 <ul>
     <li><a href="AddStudent.php">Add Student</li>
-    <li><a href="">Add Staff Member</li>
+    <li><a href="AddStaffMember.php">Add Staff Member</li>
     <li><a href="" >Exit</li>
 </ul>
 </body>
